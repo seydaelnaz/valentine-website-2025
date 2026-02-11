0d6cf2a3e15aed25899910601684400418bde664
seydaelnaz <seydaelnaz@gmail.com>
2026-02-11 14:00:04 +0100

    Updatejconfig.js
    Signed-off-by: seydaelnaz <seydaelnaz@gmail.com>

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "💝 💞 💕 Will you be on my side forever? 💖 💞 💝",
+    pageTitle: "Will you be on my side forever? 💖 💞 💝",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
